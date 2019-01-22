04a67bc551874c001e3f9ef98f5a38cc7f43d508
Matt Turnbull <matt@iamturns.com>
2019-01-23 07:42:09 +1100

    refactor: do not prefix all logged messages

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,9 +1,8 @@
 const chalk = require("chalk")
 
-const logPrefix = chalk.magenta("[create-exposed-app]")
 const errorPrefix = chalk.red("[ERROR]")
 
 // eslint-disable-next-line no-console
-export const logMessage = (...args: any[]) => console.log(logPrefix, ...args)
+export const logMessage = (...args: any[]) => console.log(...args)
 
 export const logError = (...args: any[]) => logMessage(errorPrefix, ...args)
