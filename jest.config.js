0d9e879e1895ee8b860c37c0004e91e07d1e6474
Matt Turnbull <matt@iamturns.com>
2019-01-30 20:24:36 +1100

    Upgrade Jest, tweak configs

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,4 @@
 module.exports = {
   collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}", "!src/**/*.d.ts"],
-  moduleFileExtensions: ["js", "ts", "tsx", "json", "jsx"],
   testEnvironment: "node",
-  testMatch: [
-    "<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}",
-    "<rootDir>/src/**/?(*.)(spec|test).{js,jsx,ts,tsx}",
-  ],
-  transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$"],
-  transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
-  },
 }
