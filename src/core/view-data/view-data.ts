b02475088c55f9ebcfff631c61853b3e54cca53b
Matt Turnbull <matt@iamturns.com>
2019-02-14 07:43:20 +1100

    fix: utils should not reference core

--- src/core/view-data/view-data.ts
+++ src/core/view-data/view-data.ts
@@ -3,7 +3,9 @@ import { getGitHubRepoUrl } from "./github"
 import { getMarkdownAuthor } from "./markdown-author"
 import { getNpmAuthor } from "./npm-author"
 
-export interface ViewData extends AppOptions {
+export interface ViewData
+  extends Record<string, string | undefined>,
+    AppOptions {
   gitHubRepoUrl: string
   markdownAuthor: string
   npmAuthor: string
--- src/utils/view.ts
+++ src/utils/view.ts
@@ -2,14 +2,14 @@ import fs from "fs"
 
 import ejs from "ejs"
 
-import { ViewData } from "../core/view-data/view-data"
-
-export const renderView = (view: string, data: ViewData): string =>
-  ejs.render(view, data)
+export const renderView = (
+  view: string,
+  data: Record<string, string | undefined>,
+): string => ejs.render(view, data)
 
 export const renderViewOnFile = async (
   filePath: string,
-  data: ViewData,
+  data: Record<string, string | undefined>,
 ): Promise<void> => {
   const fileContents = fs.readFileSync(filePath, "utf8")
   const view = await renderView(fileContents, data)
