923ad6ed06f29f4ca21ed7a69a91b3669488b098
Matt Turnbull <matt@iamturns.com>
2019-04-03 20:32:35 +1100

    feat: ignore tests in dist directory (#81)

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}", "!src/**/*.d.ts"],
   testEnvironment: "node",
-  testPathIgnorePatterns: ["/examples/", "/node_modules/"],
+  testPathIgnorePatterns: ["/dist/", "/examples/", "/node_modules/"],
 }
