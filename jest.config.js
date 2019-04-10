8a3d0ed170f6030dda63309f6858f97e97596173
Matt Turnbull <matt@iamturns.com>
2019-04-10 20:24:24 +1000

    fix: jest tests outside src directory no longer run (#106)

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}", "!src/**/*.d.ts"],
+  roots: ["<rootDir>/src/"],
   testEnvironment: "node",
   testPathIgnorePatterns: ["/dist/", "/examples/", "/node_modules/"],
 }
